f353b55bfbd6db8159ff392587f00a99a9b1d62a
Antonio Jerez Guillén <ajerez@freepik.com>
2023-02-19 21:38:16 +0100

    feat: initial commit

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("ajerez.core.options")
+require("ajerez.core.colorscheme")
+require("ajerez.core.keymaps")
+require("ajerez.plugins.plugins-setup")
---
+++ lua/ajerez/core/options.lua
@@ -0,0 +1,31 @@
+local opt = vim.opt 
+
+-- line numbers
+--
+opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.hlsearch = true
+opt.ignorecase = false
+opt.incsearch = true
+
+-- line wrapping
+opt.wrap = false
+opt.smartcase = true
+
+opt.cursorline = true
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus");
+
+opt.splitright = true
+opt.splitbelow = true
